ecb3c81c0113824b1d414ff6638022fe4fbc6383
azhou96 <alilizhou96@gmail.com>
2020-04-29 23:25:18 -0400

    setup

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="azhou96-taoni",
+    name="azhou-taoni", # Replace with your own username
     version="0.0.1",
     author="Alicia Zhou, Tao Ni",
     author_email="alicia.zhou@duke.edu, tao.ni@duke.edu",
-    description="Stochastic Gradient Hamilton Monte Carlo",
+    description="SGHMC package",
     url="https://github.com/azhou96/STA663-Final-Project-SGHMC",
     packages=setuptools.find_packages(),
     classifiers=[
